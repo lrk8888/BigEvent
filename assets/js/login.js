bc6f43f8815b60313c7d0436c7600817c949435e
linruikai <lrk895622@icloud.com>
2022-03-30 12:11:26 +0800

    完成了主页功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-30 07:32:47
- * @LastEditTime: 2022-03-30 10:33:36
+ * @LastEditTime: 2022-03-30 12:04:21
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /BigEvent/assets/js/login.js
